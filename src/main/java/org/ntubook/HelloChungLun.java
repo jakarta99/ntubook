7a1c3884e99ef651eeb8894e8907e6fbdaec00f1
謝中綸 <user@192.168.1.125>
2018-03-26 20:32:59 +0800

    12

--- src/main/java/org/ntubook/HelloChungLun.java
+++ src/main/java/org/ntubook/HelloChungLun.java
@@ -3,7 +3,7 @@ package org.ntubook;
 public class HelloChungLun {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		System.out.println("Hello ChungLun");
 	}
 
--- src/main/java/org/ntubook/HelloFangchun.java
+++ src/main/java/org/ntubook/HelloFangchun.java
@@ -5,6 +5,7 @@ public class HelloFangchun {
 	public static void main(String[] args) {
 		
 		System.out.println("HelloFangchun");
+		System.out.println("Hello ChungLun");
 	}
 
 }
