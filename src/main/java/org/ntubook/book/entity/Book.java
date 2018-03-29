86f363839eea9e414312191e23eff5cc3181dc2e
user <user@140.112.220.204>
2018-03-29 20:11:53 +0800

    Book

---
+++ src/main/java/org/ntubook/book/entity/Book.java
@@ -0,0 +1,43 @@
+package org.ntubook.book.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "BOOK")
+public class Book {
+	@Id
+	@Column(name = "ID")
+	private Long id;
+	@Column(name = "NAME")
+	private String name;
+	@Column(name = "PRICE")
+	private Integer price;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getPrice() {
+		return price;
+	}
+
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+	
+}
