472c54ab166801c3aa19f34de94c6cbe1b92505c
pengchen <pengchen@192.168.1.127>
2018-03-29 20:34:56 +0800

    chen

---
+++ src/main/java/org/ntubook/shelf/entity/Shelf.java
@@ -0,0 +1,44 @@
+package org.ntubook.shelf.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "SHELF")
+public class Shelf {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "ID")
+	private Long id;
+	@Column(name = "NAME")
+	private String name;
+	@Column(name = "HEIGHT")
+	private Integer height;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getHeight() {
+		return height;
+	}
+	public void setHeight(Integer height) {
+		this.height = height;
+	}
+	
+	
+	
+	
+
+}
