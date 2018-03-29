33b4611b2ff12ea262cdc7dd168ec5a44365a2f7
user <user@140.112.220.204>
2018-03-29 20:20:23 +0800

    new

--- src/main/java/org/ntubook/book/entity/Book.java
+++ src/main/java/org/ntubook/book/entity/Book.java
@@ -2,6 +2,8 @@ package org.ntubook.book.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -9,6 +11,7 @@ import javax.persistence.Table;
 @Table(name = "BOOK")
 public class Book {
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(name = "ID")
 	private Long id;
 	@Column(name = "NAME")
@@ -39,5 +42,5 @@ public class Book {
 	public void setPrice(Integer price) {
 		this.price = price;
 	}
-	
+
 }
