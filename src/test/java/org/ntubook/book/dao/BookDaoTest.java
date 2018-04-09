d0615a84ecf1f641626b18f09dda7fe8924e5c9c
user <user@192.168.1.51>
2018-04-09 20:44:16 +0800

    change to BookDaoTest

--- src/test/java/org/ntubook/book/dao/BookDaoTest.java
+++ src/test/java/org/ntubook/book/dao/BookDaoTest.java
@@ -1,5 +1,8 @@
 package org.ntubook.book.dao;
 
+import java.util.List;
+
+import org.assertj.core.util.Lists;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.ntubook.book.entity.Book;
@@ -14,11 +17,55 @@ public class BookDaoTest {
 	private BookDao bookDao;
 
 	@Test
-	public void testFindAll() {
+	public void testCrud() {
+		//findAll
+		List<Book> books = Lists.newArrayList(bookDao.findAll()); 
+		for (Book book:books) {
+			System.out.println(book);
+		}
+		System.out.println("===================================================");
+		//insert
 		Book book1 = new Book();
 		book1.setPrice(100);
 		book1.setName("Calculate");
-		book1.setId(50505050505L);
+		
+		bookDao.save(book1);
+		//findAll
+		books = Lists.newArrayList(bookDao.findAll()); 
+		for (Book book:books) {
+			System.out.println(book);
+		}
+		System.out.println("===================================================");
+		//update
+		Book book2 = new Book();
+		book2.setId(1L);
+		book2.setName("Economics");
+		book2.setPrice(500);
+		
+		bookDao.save(book2);
+		//findAll
+		books = Lists.newArrayList(bookDao.findAll()); 
+		for (Book book:books) {
+			System.out.println(book);
+		}
+		System.out.println("===================================================");
+		//delete
+		Book book3 = new Book();
+		book3.setId(1L);
+		bookDao.delete(book3);
+		
+		//findAll
+		books = Lists.newArrayList(bookDao.findAll()); 
+		for (Book book:books) {
+			System.out.println(book);
+		}
+		System.out.println("===================================================");
+		
+		
+		/*
+		Book book1 = new Book();
+		book1.setPrice(100);
+		book1.setName("Calculate");
 		
 		bookDao.save(book1);
 		
@@ -35,6 +82,7 @@ public class BookDaoTest {
 		for(Book book:books) {
 			System.out.println(book);
 		}
+		*/
 	}
 
 }
