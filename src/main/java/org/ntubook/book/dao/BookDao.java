99ba2714178616dea2e996ce305e017933f638d8
user <user@192.168.1.51>
2018-04-09 22:12:47 +0800

    0409 BookDaoTest

--- src/main/java/org/ntubook/book/dao/BookDao.java
+++ src/main/java/org/ntubook/book/dao/BookDao.java
@@ -1,8 +1,11 @@
 package org.ntubook.book.dao;
 
+import java.util.List;
+
 import org.ntubook.book.entity.Book;
 import org.springframework.data.repository.CrudRepository;
 
 public interface BookDao extends CrudRepository<Book,Long> {
 
+	public List<Book> findByNameLike(String nameLike);
 }
