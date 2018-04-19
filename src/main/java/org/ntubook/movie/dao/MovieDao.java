9048563d42cbf9a5ef76ad16e1455edfa482f790
謝中綸 <user@140.112.220.123>
2018-04-19 16:58:30 +0800

    Movie: Spring test

--- src/main/java/org/ntubook/movie/dao/MovieDao.java
+++ src/main/java/org/ntubook/movie/dao/MovieDao.java
@@ -6,6 +6,11 @@ import org.ntubook.movie.entity.Movie;
 import org.springframework.data.repository.CrudRepository;
 
 public interface MovieDao extends CrudRepository<Movie,Long> {
-
-	public List<Movie> findByYearLessThanEqual(Integer yearlessthanequal);
+	
+	public List<Movie> findByNameLike(String nameLike);
+	public List<Movie> findByYearLessThanEqual(Integer yearLessThanEqual);
+	public List<Movie> findByTypeLike(String typeLike);
+	public List<Movie> findByScoreMoreThanEqual(Float scoreMoreThanEqual);
+	public List<Movie> findByCountryLike(String countryLike);
+	public List<Movie> findByYearAndScoreMoreThanEqual(Integer year, Float scoreMoreThanEqual);
 }
--- src/test/java/org/ntubook/movie/dao/MovieDaoTest.java
+++ src/test/java/org/ntubook/movie/dao/MovieDaoTest.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.assertj.core.util.Lists;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.ntubook.book.entity.Book;
 import org.ntubook.movie.entity.Movie;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -25,22 +26,59 @@ public class MovieDaoTest {
 		}
 		System.out.println("===========================================================");
 		
+		//findOne
+		System.out.println("------------------**findByNameLike**------------------");
+		movies = movieDao.findByNameLike("C%");
+		for (Movie movie:movies) {
+			System.out.println(movie);
+		}
+				
+		System.out.println("---------------**findByYearLessThanEqual**----------------");
+		movies = movieDao.findByYearLessThanEqual(2017);
+		for (Movie movie:movies) {
+			System.out.println(movie);
+		}
+				
+		System.out.println("---------------**findByTypeLike**----------------");
+		movies = movieDao.findByTypeLike("A%");
+		for (Movie movie:movies) {
+			System.out.println(movie);
+		}
+				
+		System.out.println("---------------**findByScoreMoreThanEqual**----------------");
+		movies = movieDao.findByScoreMoreThanEqual(6f);
+		for (Movie movie:movies) {
+			System.out.println(movie);
+		}
+				
+		System.out.println("---------------**findByCountryLike**----------------");
+		movies = movieDao.findByCountryLike("A%");
+		for (Movie movie:movies) {
+			System.out.println(movie);
+		}
+				
+		System.out.println("---------------**findByYearAndScoreMoreThanEqual**----------------");
+		movies = movieDao.findByYearAndScoreMoreThanEqual(2017, 6f);
+		for (Movie movie:movies) {
+			System.out.println(movie);
+		}
+		
 		//insert
 		Movie movie1 = new Movie();
-		movie1.setType("Action");
+		movie1.setType("Action".toUpperCase());
 		movie1.setName("Doctor Strange".toUpperCase());
 		movie1.setYear(2016);
 		movie1.setScore(7.2f);
-		movie1.setCountry("France");
+		movie1.setCountry("France".toUpperCase());
 		
 		movieDao.save(movie1);
 		
 		Movie movie4 = new Movie();
-		movie4.setType("Adventure");
+		movie4.setType("Adventure".toUpperCase());
 		movie4.setName("Jumanji".toUpperCase());
 		movie4.setYear(2017);
 		movie4.setScore(6.4f);
-		movie4.setCountry("USA");
+		movie4.setCountry("USA".toUpperCase());
 				
 		movieDao.save(movie4);
 		
@@ -54,21 +92,21 @@ public class MovieDaoTest {
 		//update
 		Movie movie2 = new Movie();
 		movie2.setId(1L);
-		movie2.setType("Horror");
+		movie2.setType("Horror".toUpperCase());
 		movie2.setName("A Quiet Place".toUpperCase());
 		movie2.setYear(2018);
 		movie2.setScore(6.9f);
-		movie2.setCountry("USA");
+		movie2.setCountry("USA".toUpperCase());
 		
 		movieDao.save(movie2);
 		
 		Movie movie5 = new Movie();
 		movie5.setId(2L);
-		movie5.setType("Adventure");
+		movie5.setType("Adventure".toUpperCase());
 		movie5.setName("Foresee".toUpperCase());
 		movie5.setYear(2017);
 		movie5.setScore(6.4f);
-		movie5.setCountry("USA");
+		movie5.setCountry("USA".toUpperCase());
 		
 		movieDao.save(movie5);
 		
@@ -99,56 +137,56 @@ public class MovieDaoTest {
 		
 		/*
 		Movie movie1 = new Movie();
-		movie1.setType("Comedy");
-		movie1.setName("Penny Pincher");
+		movie1.setType("Comedy".toUpperCase());
+		movie1.setName("Penny Pincher".toUpperCase());
 		movie1.setYear(2016);
 		movie1.setScore(7.2f);
-		movie1.setCountry("France");
+		movie1.setCountry("France".toUpperCase());
 		
 		movieDao.save(movie1);
 		
 		Movie movie2 = new Movie();
-		movie2.setType("Horror");
-		movie2.setName("A Quiet Place");
+		movie2.setType("Horror".toUpperCase());
+		movie2.setName("A Quiet Place".toUpperCase());
 		movie2.setYear(2018);
 		movie2.setScore(6.9f);
-		movie2.setCountry("USA");
+		movie2.setCountry("USA".toUpperCase());
 		
 		movieDao.save(movie2);
 		
 		Movie movie3 = new Movie();
-		movie3.setType("Animated");
-		movie3.setName("Zootopia");
+		movie3.setType("Animated".toUpperCase());
+		movie3.setName("Zootopia".toUpperCase());
 		movie3.setYear(2016);
 		movie3.setScore(7.9f);
-		movie3.setCountry("USA");
+		movie3.setCountry("USA".toUpperCase());
 		
 		movieDao.save(movie3);
 		
 		Movie movie4 = new Movie();
-		movie4.setType("Adventure");
-		movie4.setName("Jumanji");
+		movie4.setType("Adventure".toUpperCase());
+		movie4.setName("Jumanji".toUpperCase());
 		movie4.setYear(2017);
 		movie4.setScore(6.4f);
-		movie4.setCountry("USA");
+		movie4.setCountry("USA".toUpperCase());
 		
 		movieDao.save(movie4);
 		
 		Movie movie5 = new Movie();
-		movie5.setType("Thriller");
-		movie5.setName("Miss Sloane");
+		movie5.setType("Thriller".toUpperCase());
+		movie5.setName("Miss Sloane".toUpperCase());
 		movie5.setYear(2016);
 		movie5.setScore(9.1f);
-		movie5.setCountry("USA and France");
+		movie5.setCountry("USA and France".toUpperCase());
 		
 		movieDao.save(movie5);
 
 		Movie movie6 = new Movie();
-		movie6.setType("Drama");
-		movie6.setName("Dangal");
+		movie6.setType("Drama".toUpperCase());
+		movie6.setName("Dangal".toUpperCase());
 		movie6.setYear(2016);
 		movie6.setScore(9.2f);
-		movie6.setCountry("India");
+		movie6.setCountry("India".toUpperCase());
 		
 		movieDao.save(movie6);
 		
