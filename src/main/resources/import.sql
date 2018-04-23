6c005a3ae78cb7eb9822e75319f673a696e64c86
Gary Lee <jakarta99@gmail.com>
2018-04-24 00:18:45 +0800

    initialize some data in Book insertion for testing

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO BOOK(NAME, PRICE, PROFESSOR) VALUES('It Ends With Her', '600', 'Brianna Labuskes');
+INSERT INTO BOOK(NAME, PRICE, PROFESSOR) VALUES('When Never Comes', '580', 'Barbara Davis');
+INSERT INTO BOOK(NAME, PRICE, PROFESSOR) VALUES('An American Princess:The Many Lives of Allene Tew', '700', 'Annejet van der Zijl');
