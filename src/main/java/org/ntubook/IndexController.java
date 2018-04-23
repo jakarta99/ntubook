a242bd6ac182fe756bba7a21994ce49893d5a9df
Gary Lee <jakarta99@gmail.com>
2018-04-24 00:26:17 +0800

    Make a IndexController for LandingPage

---
+++ src/main/java/org/ntubook/IndexController.java
@@ -0,0 +1,13 @@
+package org.ntubook;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class IndexController {
+
+	public String indexPage() {
+		return "/index";
+	}
+}
